9446ff70eccc373cb36c38698555ebdca27a35a3
Nitesh Ahiniya <niteshahiniya2.0@gmail.com>
2026-06-13 01:30:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const contactRoutes = require("./routes/contactRoutes");
-const feedbackRoutes = require("./routes/feedbackRoutes"); 
+const feedbackRoutes = require("./routes/feedbackRoutes");
 const memoryRoutes = require("./routes/memoryRoutes");
 
 const app = express();
@@ -14,15 +14,14 @@ const app = express();
 connectDB();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api", contactRoutes);
-app.use("/api", feedbackRoutes); 
+app.use("/api", feedbackRoutes);
 app.use("/api", memoryRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server Running On ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
 });
+
+module.exports = app;
